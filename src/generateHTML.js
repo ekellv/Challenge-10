172ae6d48a0b3b282dafe09ba63df8e0e1729615
Erin V <erinkvoelker@gmail.com>
2022-08-01 11:33:24 -0400

    added code to generate HTML & tested to make sure it works correctly

---
+++ src/generateHTML.js
@@ -0,0 +1,118 @@
+// function to connect data from inquirer prompts to the actual HTML output of the team member cards
+const generateTeam = team => { 
+
+    // create Manager card html elements
+    const generateManager = manager => {
+        return `
+        <div class="card employee-card">
+        <div class="card-header bg-success text-white">
+            <h2 class="card-title">${manager.name}</h2>
+            <h3 class="card-title"><i class="fas fa-mug-hot mr-2"></i>${manager.getRole()}</h3>
+        </div>
+        <div class="card-body">
+        <ul class="list-group">
+            <li class="list-group-item">ID: ${manager.id}</li>
+            <li class="list-group-item">Email: <a href="mailto:${manager.email}">${manager.email}</a></li>
+            <li class="list-group-item">Office number: ${manager.officeNum}</li>
+        </ul>
+        </div>
+        </div>
+        `;
+    };
+
+    // create Engineer card html elements
+    const generateEngineer = engineer => {
+        return `
+        <div class="card employee-card">
+        <div class="card-header bg-success text-white">
+            <h2 class="card-title">${engineer.name}</h2>
+            <h3 class="card-title"><i class="fas fa-glasses mr-2"></i>${engineer.getRole()}</h3>
+        </div>
+        <div class="card-body">
+        <ul class="list-group">
+            <li class="list-group-item">ID: ${engineer.id}</li>
+            <li class="list-group-item">Email: <a href="mailto:${engineer.email}">${engineer.email}</a></li>
+            <li class="list-group-item">GitHub: <a href="https://github.com/${engineer.github}" target="_blank" rel="noopener noreferrer">${engineer.github}</a></li>
+        </ul>
+        </div>
+        </div>
+        `;
+    };
+
+    // create Intern card html elements 
+    const generateIntern = intern => {
+        return `
+        <div class="card employee-card">
+            <div class="card-header bg-success text-white">
+                <h2 class="card-title">${intern.name}</h2>
+                <h3 class="card-title"><i class="fas fa-user-graduate mr-2"></i>${intern.getRole()}</h3>
+            </div>
+            <div class="card-body">
+                <ul class="list-group">
+                    <li class="list-group-item">ID: ${intern.id}</li>
+                    <li class="list-group-item">Email: <a href="mailto:${intern.email}">${intern.email}</a></li>
+                    <li class="list-group-item">School: ${intern.school}</li>
+                </ul>
+            </div>
+        </div>
+        `;
+    };
+
+    // empty html array to combine all elements 
+    const html = [];
+
+    html.push(team
+        .filter(employee => employee.getRole() === "Manager")
+        .map(manager => generateManager(manager))
+    );
+    html.push(team
+        .filter(employee => employee.getRole() === "Engineer")
+        .map(engineer => generateEngineer(engineer))
+        .join("")
+    );
+    html.push(team
+        .filter(employee => employee.getRole() === "Intern")
+        .map(intern => generateIntern(intern))
+        .join("")
+    );
+    
+    return html.join("");
+}
+
+// export function to generate final html page
+module.exports = team => {
+    return `
+    <!DOCTYPE html>
+    <html lang="en">
+    <head>
+        <meta charset="UTF-8" />
+        <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+        <meta http-equiv="X-UA-Compatible" content="ie=edge" />
+        <title>My Tech Team</title>
+        <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css"
+            integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
+        <link rel="stylesheet" href="style.css">
+        <link rel="preconnect" href="https://fonts.googleapis.com">
+        <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+        <link href="https://fonts.googleapis.com/css2?family=Rubik:ital,wght@0,300;0,400;0,500;0,600;0,700;0,800;0,900;1,300;1,400;1,500;1,600;1,700;1,800;1,900&display=swap" rel="stylesheet">
+        <script src="https://kit.fontawesome.com/c502137733.js"></script>
+    </head>
+    <body>
+        <div class="container-fluid">
+            <div class="row">
+                <div class="col-12 jumbotron mb-3 team-heading bg-info">
+                    <h1 class="text-center text-white">My Team</h1>
+                </div>
+            </div>
+        </div>
+        <div class="container">
+            <div class="row">
+                <div class="row team-area col-12 d-flex justify-content-center">
+                    ${generateTeam(team)}
+                </div>
+            </div>
+        </div>
+    </body>
+    </html>
+    `;
+};
