8d6f8c803f8a4594b20f65cab4f1422d307c67a0
Erin V <erinkvoelker@gmail.com>
2022-08-02 09:00:26 -0400

    fixed html spacing

--- src/generateHTML.js
+++ src/generateHTML.js
@@ -4,18 +4,18 @@ const generateTeam = team => {
     // create Manager card html elements
     const generateManager = manager => {
         return `
-        <div class="card employee-card">
-        <div class="card-header bg-success text-white">
-            <h2 class="card-title">${manager.name}</h2>
-            <h3 class="card-title"><i class="fas fa-mug-hot mr-2"></i>${manager.getRole()}</h3>
-        </div>
-        <div class="card-body">
-        <ul class="list-group">
-            <li class="list-group-item">ID: ${manager.id}</li>
-            <li class="list-group-item">Email: <a href="mailto:${manager.email}">${manager.email}</a></li>
-            <li class="list-group-item">Office number: ${manager.officeNum}</li>
-        </ul>
-        </div>
+        <div class="card team-card">
+            <div class="card-header bg-primary text-white">
+                <h2 class="card-title">${manager.name}</h2>
+                <h3 class="card-title"><i class="fas fa-mug-hot mr-2"></i>${manager.getRole()}</h3>
+            </div>
+            <div class="card-body">
+                <ul class="list-group">
+                    <li class="list-group-item">ID: ${manager.id}</li>
+                    <li class="list-group-item">Email: <a href="mailto:${manager.email}">${manager.email}</a></li>
+                    <li class="list-group-item">Office number: ${manager.officeNum}</li>
+                </ul>
+            </div>
         </div>
         `;
     };
@@ -23,38 +23,38 @@ const generateTeam = team => {
     // create Engineer card html elements
     const generateEngineer = engineer => {
         return `
-        <div class="card employee-card">
-        <div class="card-header bg-success text-white">
-            <h2 class="card-title">${engineer.name}</h2>
-            <h3 class="card-title"><i class="fas fa-glasses mr-2"></i>${engineer.getRole()}</h3>
-        </div>
-        <div class="card-body">
-        <ul class="list-group">
-            <li class="list-group-item">ID: ${engineer.id}</li>
-            <li class="list-group-item">Email: <a href="mailto:${engineer.email}">${engineer.email}</a></li>
-            <li class="list-group-item">GitHub: <a href="https://github.com/${engineer.github}" target="_blank" rel="noopener noreferrer">${engineer.github}</a></li>
-        </ul>
-        </div>
-        </div>
+            <div class="card team-card">
+                <div class="card-header bg-success text-white">
+                    <h2 class="card-title">${engineer.name}</h2>
+                    <h3 class="card-title"><i class="fas fa-glasses mr-2"></i>${engineer.getRole()}</h3>
+                </div>
+                <div class="card-body">
+                    <ul class="list-group">
+                        <li class="list-group-item">ID: ${engineer.id}</li>
+                        <li class="list-group-item">Email: <a href="mailto:${engineer.email}">${engineer.email}</a></li>
+                        <li class="list-group-item">GitHub: <a href="https://github.com/${engineer.github}" target="_blank" rel="noopener noreferrer">${engineer.github}</a></li>
+                    </ul>
+                </div>
+            </div>
         `;
     };
 
     // create Intern card html elements 
     const generateIntern = intern => {
         return `
-        <div class="card employee-card">
-            <div class="card-header bg-success text-white">
-                <h2 class="card-title">${intern.name}</h2>
-                <h3 class="card-title"><i class="fas fa-user-graduate mr-2"></i>${intern.getRole()}</h3>
+            <div class="card team-card">
+                <div class="card-header bg-warning text-white">
+                    <h2 class="card-title">${intern.name}</h2>
+                    <h3 class="card-title"><i class="fas fa-user-graduate mr-2"></i>${intern.getRole()}</h3>
+                </div>
+                <div class="card-body">
+                    <ul class="list-group">
+                        <li class="list-group-item">ID: ${intern.id}</li>
+                        <li class="list-group-item">Email: <a href="mailto:${intern.email}">${intern.email}</a></li>
+                        <li class="list-group-item">School: ${intern.school}</li>
+                    </ul>
+                </div>
             </div>
-            <div class="card-body">
-                <ul class="list-group">
-                    <li class="list-group-item">ID: ${intern.id}</li>
-                    <li class="list-group-item">Email: <a href="mailto:${intern.email}">${intern.email}</a></li>
-                    <li class="list-group-item">School: ${intern.school}</li>
-                </ul>
-            </div>
-        </div>
         `;
     };
 
@@ -89,8 +89,7 @@ module.exports = team => {
         <meta name="viewport" content="width=device-width, initial-scale=1.0" />
         <meta http-equiv="X-UA-Compatible" content="ie=edge" />
         <title>My Tech Team</title>
-        <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css"
-            integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
+        <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.1.3/dist/css/bootstrap.min.css" integrity="sha384-MCw98/SFnGE8fJT3GXwEOngsV7Zt27NXFoaoApmYm81iuXoPkFOJwJ8ERdknLPMO" crossorigin="anonymous">
         <link rel="stylesheet" href="style.css">
         <link rel="preconnect" href="https://fonts.googleapis.com">
         <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
@@ -101,7 +100,7 @@ module.exports = team => {
         <div class="container-fluid">
             <div class="row">
                 <div class="col-12 jumbotron mb-3 team-heading bg-info">
-                    <h1 class="text-center text-white">My Team</h1>
+                    <h1 class="text-center text-white">My Tech Team</h1>
                 </div>
             </div>
         </div>
